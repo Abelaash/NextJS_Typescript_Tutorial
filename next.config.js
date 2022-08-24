0149095155e6bbffc2747edf041035bba799e7d2
Abelaash Giritharan <50346809+Abelaash@users.noreply.github.com>
2022-08-23 22:34:31 -0400

    add files to repo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images:{
+    domains: ["rickandmortyapi.com"],
+    loader: "custom",
+    path: "/",
+  },
 }
 
 module.exports = nextConfig
