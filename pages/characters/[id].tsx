6908968be5f301cb7bc3d57b37b9d5c4ed2b1095
Abelaash Giritharan <50346809+Abelaash@users.noreply.github.com>
2022-08-24 10:00:48 -0400

    add all files to repo

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -1,41 +1,45 @@
+import { GetServerSideProps } from "next";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import imageLoader from "../../imageLoader";
-import { Character, GetCharacterResults } from "../../types";
+import { Character } from "../../types";
+import styles from "../../styles/Character.module.css";
+import Layout from "../../components/Layout";
 
-function CharacterPage({character} : {
-    character: Character
-}){
-    return <div>
-        <h1>{character.name}</h1>
+function CharacterPage({ character }: { character: Character }) {
+  const router = useRouter();
 
-        <Image
-              loader={imageLoader}
-              unoptimized
-              src={character.image}
-              alt={character.name}
-              width="200"
-              height="200"
-            />
+  return (
+    <div className={styles.container}>
+      <h1>{character.name}</h1>
+
+      <Image
+        loader={imageLoader}
+        unoptimized
+        src={character.image}
+        alt={character.name}
+        width="200px"
+        height="200px"
+      />
     </div>
+  );
 }
 
-export async function getStaticPaths() {
-  const res = await fetch("https://rickandmortyapi.com/api/character");
-  const { results }: GetCharacterResults = await res.json();
+CharacterPage.getLayout = function getLayout(page: typeof CharacterPage){
+  return <Layout>{page}</Layout>
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/character/${context.query.id}`
+  );
+  const character = await res.json();
+
   return {
-    paths: results.map((character) => {
-      return {
-        params: { id: String(character.id) },
-      };
-    }),
+    props: {
+      character,
+    },
   };
-}
-
-export async function getStaticProps({params} : {params: {id: string}}){
-    const res = await fetch(
-        `https://rickandmortyapi.com/api/character/${params.id}`
-    );
-    const character = res.json()
-}
+};
 
 export default CharacterPage;
